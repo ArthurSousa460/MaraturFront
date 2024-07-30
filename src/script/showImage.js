4ea5cc9bfa9fa362d043d12df13669f3f1c6ff75
ArthurSousa460 <arthurprogrammer460@gmail.com>
2024-07-30 11:13:17 -0300

    update titles from html files

--- src/script/showImage.js
+++ src/script/showImage.js
@@ -1,4 +1,5 @@
 const imageBox = document.querySelector(".image-box");
+const imageName = document.querySelector("#image-name");
 
 function getURLParams(){
     const URLParams = new URLSearchParams(window.location.search);
@@ -22,8 +23,8 @@ async function getImages(){
     image.setAttribute("class", "attractive-image");
     backButton.setAttribute("class", "back-button");
     backButton.setAttribute("href", `showAttractive.html?id=${data[0].cod_attractive}&origin=searchAttractive.html`);
-
-
+    
+    imageName.innerText = data[0].name;
     backButton.innerText = "Voltar";
 
     imageBox.appendChild(image);
